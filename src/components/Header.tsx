b860914d17b9c871c2562b7982ce441d01dbf201
Lafaiete do Vale <lafa.vale@gmail.com>
2023-04-12 23:59:17 -0300

    Header componentized

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,10 @@ import {
   RiSearchLine,
   RiUserAddLine,
 } from "react-icons/ri";
+import { Profile } from "./Header/Profile";
+import { NotificationsNav } from "./Header/NotificationsNav";
+import { SearchBox } from "./Header/SearchBox";
+import { Logo } from "./Header/Logo";
 
 export function Header() {
   return (
@@ -17,66 +21,13 @@ export function Header() {
       px="6"
       align="center"
     >
-      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
-        dashgo
-        <Text as="span" ml="1" color="pink.500">
-          .
-        </Text>
-      </Text>
+      <Logo />
 
-      <Flex
-        as="label"
-        flex="1"
-        py="4"
-        px="8"
-        ml="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        bg="gray.800"
-        borderRadius="full"
-      >
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          px="4"
-          mr="4"
-          placeholder="Buscar na plataforma"
-          _placeholder={{ color: "gray.400" }}
-        />
-
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex>
+      <SearchBox />
 
       <Flex align="center" ml="auto">
-        <HStack
-          spacing="8"
-          mx="8"
-          pr="8"
-          py="1"
-          color="gray.300"
-          borderRightWidth={1}
-          borderColor="gray.700"
-        >
-          <Icon as={RiNotificationLine} fontSize="20" />
-          <Icon as={RiUserAddLine} fontSize="20" />
-        </HStack>
-
-        <Flex align="center">
-          <Box mr="4" textAlign="right">
-            <Text>Lafaiete do Vale</Text>
-            <Text color="gray.300" fontSize="small">
-              lafa.vale@gmail.com
-            </Text>
-          </Box>
-
-          <Avatar
-            size="md"
-            name="Lafaiete Vale"
-            src="https://github.com/lafadovale.png"
-          />
-        </Flex>
+        <NotificationsNav />
+        <Profile />
       </Flex>
     </Flex>
   );
